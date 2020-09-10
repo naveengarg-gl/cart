996bf85ff42f490c8284a0b728a3d64fb02f6e1f
Naveen <naveen.garg@globallogic.com>
2020-09-10 11:16:04 +0530

    session-3

--- src/main/java/com/training/ms/cart/controller/CartController.java
+++ src/main/java/com/training/ms/cart/controller/CartController.java
@@ -3,6 +3,8 @@ package com.training.ms.cart.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +18,15 @@ import com.training.ms.cart.repo.CartRepository;
 
 
 
-
+@RefreshScope
 @RestController
 public class CartController {
 	
 	@Autowired
 	CartRepository cartRepository;
+	
+	@Value("${cart.name}")
+    private String name;
 	
 	@Autowired
 	ProducerCart producerCart;
@@ -54,7 +59,11 @@ public class CartController {
 	    (@PathVariable int customerId){
 		 logger.info("Cart Service Port="+Integer.parseInt(environment.getProperty("local.server.port")));
 		 CartDetail cartDetail = cartRepository.findByCustomerId(customerId);
-		
+		 if(cartDetail==null)
+		 {
+			 cartDetail = new CartDetail();
+		 }
+		 cartDetail.setProductName(name);
 	    return cartDetail;
 	  
 	} 
